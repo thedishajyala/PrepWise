634e71cb269251aa5032690c21f2db3c1d7c45e9
DISHA JYALA <thedishajyala@gmail.com>
2026-01-26 20:44:14 +0530

    ignore eslint and typescript errors (for now)

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,26 +1,31 @@
-// Import the functions you need from the SDKs you need
-import { getApp, getApps, initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics, isSupported } from "firebase/analytics"; 
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCGXtx1vAtYNgcfmCPavdT-X0gi3iJXWL4",
+  authDomain: "prepwise-b329a.firebaseapp.com",
+  projectId: "prepwise-b329a",
+  storageBucket: "prepwise-b329a.firebasestorage.app",
+  messagingSenderId: "1066524028804",
+  appId: "1:1066524028804:web:3ebf5a7f4548c3abca77d1",
+  measurementId: "G-FCEHC4YQRC"
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const analytics = getAnalytics(app);
+
+// Initialize Analytics only in browser environment
+let analytics;
+if (typeof window !== "undefined") {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export { analytics };
