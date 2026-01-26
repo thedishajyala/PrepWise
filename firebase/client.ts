bf7a906fca639ecfaea00cb85efb1a87c50dd428
DISHA JYALA <thedishajyala@gmail.com>
2026-01-26 21:33:05 +0530

    fix: use env firebase config

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGXtx1vAtYNgcfmCPavdT-X0gi3iJXWL4",
-  authDomain: "prepwise-b329a.firebaseapp.com",
-  projectId: "prepwise-b329a",
-  storageBucket: "prepwise-b329a.firebasestorage.app",
-  messagingSenderId: "1066524028804",
-  appId: "1:1066524028804:web:3ebf5a7f4548c3abca77d1",
-  measurementId: "G-FCEHC4YQRC"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 // Initialize Firebase
